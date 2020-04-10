aebf3f6715ac516a04d99ac9a31a494d88b46016
Alexandru Hapco <ahapco2@scj.com>
2020-04-10 17:25:56 +0100

    tests updated with dummy file

--- FileIdCreatorTest/FileIdTest.cs
+++ FileIdCreatorTest/FileIdTest.cs
@@ -1,22 +1,35 @@
 ﻿using FileIdCreator;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace FileIdCreatorTest {
     [TestClass]
     public class FileIdTest {
 
-        const string path = "";
+        const string pathInit = "TestFile/dummy.pdf";
+        const string path = "TestFile/dummyTest.pdf";
+
+        [TestInitialize]
+        public void init() {           
+            File.Copy(pathInit, path);
+        }
+
+        [TestCleanup]
+        public void cleanUp() {
+            File.Delete(path);
+        }
 
         [TestMethod]
-        public void deleteIDTest() {           
+        public void deleteIDTest() {  
             FileId.addID(path, 1);
-            var a = FileId.getID(path);
-            Assert.AreEqual("###0000000001###", a);
+            var actualId = FileId.getID(path);
+            Assert.AreEqual("###0000000001###", actualId);
 
             FileId.deleteID(path);
-            var b = FileId.getID(path);
-            Assert.IsNull(b);
+            var shouldBeNull = FileId.getID(path);
+            Assert.IsNull(shouldBeNull);
         }
 
         [TestMethod]
@@ -24,6 +37,13 @@ namespace FileIdCreatorTest {
         public void exceptionCheck() {
             var nullPath = "";
             FileId.addID(nullPath, 1);
+        }
+
+        [TestMethod]
+        public void updateIDTest() {          
+            FileId.updateID(path, 123);
+            var actualId = FileId.getID(path);
+            Assert.AreEqual("###0000000123###", actualId);         
         }
 
     }
